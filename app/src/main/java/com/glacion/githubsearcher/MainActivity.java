19334f9ab3f0c449cf1ced077e0be32d41d8f6a5
Ahmetcan Güvendiren <ahmtcngvndrn@gmail.com>
2018-04-11 15:32:52 +0300

    Enable choice

--- app/src/main/java/com/glacion/githubsearcher/MainActivity.java
+++ app/src/main/java/com/glacion/githubsearcher/MainActivity.java
@@ -1,6 +1,7 @@
 package com.glacion.githubsearcher;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -35,48 +36,87 @@ public class MainActivity extends AppCompatActivity {
     private List<Repo> repoList;
     private TextView inputField;
     private ProgressBar progressBar;
+    private boolean reverseChecked = false;
+    private SharedPreferences.Editor editor;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         requestQueue = VolleyCore.getCore(getApplicationContext().getCacheDir()).getRequestQueue();
-        recyclerView = findViewById(R.id.repo_view);
         inputField = findViewById(R.id.query_input);
         progressBar = findViewById(R.id.progress_bar);
+        recyclerView = findViewById(R.id.repo_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         ItemOffset offset = new ItemOffset(this, R.dimen.side_margin);
         recyclerView.addItemDecoration(offset);
+        preferences = this.getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
     }
 
     // Menu related stuff.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.search, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem sortMenu = menu.findItem(R.id.sort_list);
+        getMenuInflater().inflate(R.menu.submenu_sort, sortMenu.getSubMenu());
+        Menu sortSubMenu = sortMenu.getSubMenu();
+        String sorted = preferences.getString(getString(R.string.repo_sort_key), getString(R.string.stars));
+        if (sorted.equals(getString(R.string.stars)))
+            sortSubMenu.findItem(R.id.repo_sort_stars).setChecked(true);
+        else if (sorted.equals(getString(R.string.forks)))
+            sortSubMenu.findItem(R.id.repo_sort_forks).setChecked(true);
+        else if (sorted.equals(getString(R.string.updated)))
+            sortSubMenu.findItem(R.id.repo_sort_updated).setChecked(true);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.search_button){
-            String query = inputField.getText().toString();
-            if (query.equals(""))
-                Toast.makeText(getApplicationContext(), "Empty query", Toast.LENGTH_SHORT).show();
-            else if (!isNetworkAvailable())
-                Toast.makeText(getApplicationContext(), "Device is not connected", Toast.LENGTH_SHORT).show();
-            else {
-                progressBar.setVisibility(View.VISIBLE);
-                // The list will be created from scratch to avoid the same list being used by
-                // multiple queries.
-                repoList = new LinkedList<>();
-                String url = NetworkUtils.buildURL(inputField.getText().toString());
-                JsonObjectRequest request = makeRequest(url);
-                requestQueue.add(request);
-            }
+        switch (item.getItemId()){
+            case R.id.search_button:
+                search();
+                return true;
+            case R.id.reverse_check:
+                item.setChecked(!reverseChecked);
+                reverseChecked = !reverseChecked;
+                return true;
+            case R.id.repo_sort_forks:
+                onSortSelect(item, getString(R.string.forks));
+                return true;
+            case R.id.repo_sort_stars:
+                onSortSelect(item, getString(R.string.stars));
+                return true;
+            case R.id.repo_sort_updated:
+                onSortSelect(item, getString(R.string.updated));
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void onSortSelect(MenuItem item, String selection) {
+        editor = preferences.edit();
+        item.setChecked(true);
+        editor.putString(getString(R.string.repo_sort_key), selection);
+        editor.apply();
+    }
+    private void search() {
+        String query = inputField.getText().toString();
+        if (query.equals(""))
+            Toast.makeText(getApplicationContext(), "Empty query", Toast.LENGTH_SHORT).show();
+        else if (!isNetworkAvailable())
+            Toast.makeText(getApplicationContext(), "Device is not connected", Toast.LENGTH_SHORT).show();
+        else {
+            progressBar.setVisibility(View.VISIBLE);
+            // The list will be created from scratch to avoid the same list being used by
+            // multiple queries.
+            repoList = new LinkedList<>();
+            String url = NetworkUtils.buildURL(inputField.getText().toString());
+            JsonObjectRequest request = makeRequest(url);
+            requestQueue.add(request);
+        }
+    }
     /**
      * Parses and prepares the json object returned from the background task.
      * @param response A Json Object retrieved from the operation in the background.
@@ -113,6 +153,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
             }
         };
 
@@ -132,5 +173,11 @@ public class MainActivity extends AppCompatActivity {
             activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         }
         return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+
+    @Override
+    protected void onDestroy() {
+        editor.commit();
+        super.onDestroy();
     }
 }
